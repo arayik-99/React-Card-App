e2680a143a56b72fc2fd690521e6c13d3882b44a
Ara-1999 <hovhannisyanara99@gmail.com>
2020-08-31 12:23:20 +0400

    syntax optimization

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,12 @@ import './header.scss';
 const Header = ({ addCard, sortCards }) => {
   return (
     <div className='header'>
-      <button onClick={addCard}><h2>Add Card</h2></button>
-      <button onClick={sortCards}><h2>Sort Cards</h2></button>
+      <button onClick={addCard}>
+        <h2>Add Card</h2>
+      </button>
+      <button onClick={sortCards}>
+        <h2>Sort Cards</h2>
+      </button>
     </div>
   );
 };
